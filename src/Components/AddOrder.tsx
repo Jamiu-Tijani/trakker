36fed12c035d100c3f804074ca00bc3358552f73
Mopharr <olamiquadri1@gmail.com>
2023-12-10 22:23:57 +0800

    order and checkout

--- src/Components/AddOrder.tsx
+++ src/Components/AddOrder.tsx
@@ -8,30 +8,44 @@ const AddProduct = ({ handleAddPro }: any) => {
     <div className="modal">
       <form className="catModal">
         <div className="catModalTop">
-          <h4>Add Product</h4>
+          <h4>Order</h4>
           <ImCancelCircle className="catCancel" onClick={handleAddPro} />
         </div>
         <div className="line"></div>
         <div className="catModalInput branchInput">
-          <div className="div modalDouble addProduct">
-            <input type="text" placeholder="Product Name" name="name" />
-            <input type="text" placeholder="Description" name="description" />
-          </div>
+          <h3>Product details</h3>
+          <select name="name">
+            <option value="">Select Product</option>
+            <option value="">Biscuit</option>
+            <option value="">Sweet</option>
+            <option value="">RIce</option>
+            <option value="">Card</option>
+          </select>
           <div className="div modalDouble addProduct">
             <input type="text" placeholder="Quantity" name="quantity" />
+            <input type="text" placeholder="Description" name="description" />
+          </div>
 
+          <div className="line"></div>
+          <h3>Product details</h3>
+
+          <div className="div modalDouble addProduct">
+            <input type="text" placeholder="User Name" name="unitOfMeasure" />
             <input
               type="text"
-              placeholder="Unit Of Measurment"
+              placeholder="Phone Number"
               name="unitOfMeasure"
             />
           </div>
-          <input type="text" placeholder="Unit Selling" name="selling" />
-          <input type="text" placeholder="Category" name="category" />
+          <div className="div modalDouble addProduct">
+            <input type="text" placeholder="Pick up Location" name="selling" />
+
+            <input type="text" placeholder="Drop of Location" name="category" />
+          </div>
         </div>
 
         <div className="catModalBtn">
-          <button className="catBtn1">Add Product</button>
+          <button className="catBtn1">Confirm Order</button>
           <button className="catBtn2" onClick={handleAddPro}>
             Cancel
           </button>
