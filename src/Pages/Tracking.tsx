9347637d69711fd165bb6181e9cd631cadeb03ec
Mopharr <olamiquadri1@gmail.com>
2023-12-13 06:19:49 +0800

    tracking board

--- src/Pages/Tracking.tsx
+++ src/Pages/Tracking.tsx
@@ -3,7 +3,9 @@ import TopNav from "../layouts/TopNav";
 import "../Styles/track.css";
 import "../Styles/order.css";
 import { Link } from "react-router-dom";
-import { FaRegMap } from "react-icons/fa";
+import { FaRegMap, FaUncharted } from "react-icons/fa";
+import { CiBookmarkCheck, CiCreditCard1 } from "react-icons/ci";
+import { FaListCheck } from "react-icons/fa6";
 
 const Tracking = () => {
   const orderNumber = <p style={{ color: "blue" }}>#22346</p>;
@@ -19,19 +21,42 @@ const Tracking = () => {
           <div className="progress">
             <div className="proCircle">
               <div className="ciDetails">
-                <FaRegMap />
-                <p>Sales check</p>
+                <CiBookmarkCheck style={{ fontSize: "40px" }} />
+                <p>Order </p>
               </div>
             </div>
             <div className="proLines"></div>
-            <div className="proCircle"></div>
+            <div className="proCircle">
+              <div className="ciDetails">
+                <FaUncharted style={{ fontSize: "40px" }} />
+                <p>Sales </p>
+              </div>
+            </div>
             <div className="proLines"></div>
-            <div className="proCircle"></div>
+            <div className="proCircle">
+              <div className="ciDetails">
+                <CiCreditCard1 style={{ fontSize: "40px" }} />
+                <p>Finance </p>
+              </div>
+            </div>
             <div className="proLines"></div>
-            <div className="proCircle"></div>
+            <div className="proCircle">
+              <div className="ciDetails">
+                <FaListCheck style={{ fontSize: "40px" }} />
+                <p>Order</p>
+              </div>
+            </div>
             <div className="proLines"></div>
-            <div className="proCircle"></div>
+            <div className="proCircle">
+              <div className="ciDetails">
+                <CiBookmarkCheck style={{ fontSize: "40px" }} />
+                <p>Delivered</p>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="proc">
+            
         </div>
       </div>
     </div>
