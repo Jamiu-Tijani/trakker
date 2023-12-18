c6b66d2e6f82fb6897bc89bfd7ae4ef29ca6e2b9
Mopharr <olamiquadri1@gmail.com>
2023-12-19 07:05:56 +0800

    invoice update

--- src/Pages/Invoice.tsx
+++ src/Pages/Invoice.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import "../Styles/invoice.css"
 
 const Invoice = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="invoice">
+      <div className="topIn">
+        <div>
+          <div className="topLogo">A</div>
+          <p>Invoice Number: 127</p>
+          <span>Data: 12/12/2023</span>
+        </div>
 
-export default Invoice
+        <h2>Invoice Slip</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Invoice;
